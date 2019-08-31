7354271df04329704400289fd8a419c9d98ea88b
Abhilash Pillai <abhilash.pillai@hrblock.com>
2019-08-31 14:00:54 +0530

    latest changes

--- application-new/src/app/new-return/new-return.compoenent.ts
+++ application-new/src/app/new-return/new-return.compoenent.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-new-return',
-    templateUrl: 'new-return.component.html'
+    templateUrl: 'new-return.component.html',
+    styles: [`
+        .card.card-stats{
+            cursor:pointer;
+        }
+        .card.card-stats.active{
+            opacity:.5;
+            background:lightblue;
+        }
+        .active:before{
+            content: "✓";
+            position: absolute;
+            top: 15%;
+            right: 8%;
+            z-index: 1;
+            font-size: 45px;
+            font-weight: bolder;
+        }
+    `]
+
 })
 export class NewReturnComponent implements OnInit {
 
-    constructor() { }
+    public questions = []
+
+    constructor(private http: HttpClient) { }
 
     ngOnInit() {
+        this.http.get('./assets/data/config.json')
+            .toPromise()
+            .then((data: any[]) => {
+                this.questions = data;
+            });
+    }
 
+    setActive(question: any) {
+        question.active = !question.active;
     }
 
 }
